ad0ec97d61e1b4d04eb7dd31358f95ab7364ac39
Vladon79 <vla3ik@gmail.com>
2021-11-03 16:13:40 +0300

    45_commit

--- src/Components/Profile/ProfileContainer.tsx
+++ src/Components/Profile/ProfileContainer.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { AppStoreType } from '../../redux/redux-store';
-import { ActionType, postType } from '../../types/types';
+import { ActionType, postType, stateType } from '../../types/types';
 import Profile from './Profile';
 import { Store } from 'redux';
 import { AddPostAC, NewPostChangeAC } from '../../redux/profile-reduser';
 
-type PostType = {
-  store: Store<any, ActionType>
+// type PostType = {
+//   store: Store<any, ActionType>
+// }
+
+// const ProfileContainer = (props: PostType) => {
+//   const post = useSelector<AppStoreType, Array<postType>>(state => state.postPage.postData)
+//   const NewPostMessage = useSelector<AppStoreType, string>(state => state.postPage.NewPostMessage)
+
+//   const newPostChange = (text: string) => {
+//     props.store.dispatch(NewPostChangeAC(text))
+
+//   }
+
+//   const addPost = (text: string) => {
+//     props.store.dispatch(AddPostAC(text))
+//   }
+
+//   return <Profile
+//     post={post}
+//     addPost={addPost}
+//     newPostChange={newPostChange}
+//     NewPostMessage={NewPostMessage}
+//   />
+// }
+
+const mapStateToProps = (state: AppStoreType) => {
+  return {
+    post: state.postPage.postData,
+    NewPostMessage:state.postPage.NewPostMessage
+  }
 }
 
-const ProfileContainer = (props: PostType) => {
-  const post = useSelector<AppStoreType, Array<postType>>(state => state.postPage.postData)
-  const NewPostMessage = useSelector<AppStoreType, string>(state => state.postPage.NewPostMessage)
-
-  const newPostChange = (text: string) => {
-    props.store.dispatch(NewPostChangeAC(text))
-   
+const mapDispachToProps = (dispatch:any) => {
+  return {
+    addPost:(text: string)=>{
+      dispatch(AddPostAC(text))
+    },
+    newPostChange:(text: string)=>{
+      dispatch(NewPostChangeAC(text))
+    }
   }
-
-  const addPost = (text: string) => {
-    props.store.dispatch(AddPostAC(text))
-  }
-
-  return <Profile
-    post={post}
-    addPost={addPost}
-    newPostChange={newPostChange}
-    NewPostMessage={NewPostMessage}
-  />
 }
+
+const ProfileContainer = connect(mapStateToProps, mapDispachToProps)(Profile);
 export default ProfileContainer;
