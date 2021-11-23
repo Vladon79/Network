69ca9b52fa188006c7da152b6d4f4506ad7154a2
Vladon79 <vla3ik@gmail.com>
2021-11-23 15:08:38 +0300

    56-commit

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -1,57 +1,68 @@
-import axios from "axios";
-import { userInfo } from "os";
 import React from "react";
 import { UsersType } from "../../types/types";
 import s from './Users.module.css';
 import userPhoto from './../../assents/image/user.png'
 
-type UsersPageType = {
-    friends: Array<UsersType>
-    AddFriends: (friendsID: number) => void
-    RemoveFriends: (friendsID: number) => void
-    SetFriends: (friends: Array<UsersType>) => void
+type UsersPageTypeProps = {
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    onPageChange: (p: number) => void
+    RemoveUsers: (id: number) => void
+    AddUsers: (id: number) => void
+    users: Array<UsersType>
 }
 
-const Users = (props: UsersPageType) => {
-
-    if (props.friends.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {                       
-            props.SetFriends(response.data.items)
-        });
+const Users = (props: UsersPageTypeProps) => {
+    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    const pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
-    
-
-
     return (
 
         <div className={s.div}>
+            <div>
+                {pages.map(p => {
+                    return (
+                        <span className={props.currentPage === p ? s.selectedPage : ''}
+                            onClick={() => props.onPageChange(p)}>{p}</span>
+                    )
 
-            {props.friends.map(f => <div key={f.id} className={s.friendsPage}>
-                <div className={s.AvaButton}>
-                    <div className={s.avaDiv}>
-                        <img src={f.ava ? f.ava : userPhoto} className={s.ava} />
+                })}
+
+            </div>
+
+            {
+                props.users.map(f => <div key={f.id} className={s.friendsPage}>
+                    <div className={s.AvaButton}>
+                        <div className={s.avaDiv}>
+                            <img src={f.ava ? f.ava : userPhoto} className={s.ava} />
+                        </div>
+                        <div className={s.buttonDiv}>
+                            {f.friends ?
+                                <button onClick={() => props.RemoveUsers(f.id)} className={s.button}>Remove</button>
+                                :
+                                <button onClick={() => props.AddUsers(f.id)} className={s.button}>Add</button>}
+
+                        </div>
                     </div>
-                    <div className={s.buttonDiv}>
-                        {f.friends ?
-                            <button onClick={() => props.RemoveFriends(f.id)} className={s.button}>Remove</button>
-                            : <button onClick={() => { props.AddFriends(f.id) }} className={s.button}>Add</button>}
 
+                    <div className={s.NameStatus}>
+                        <div className={s.name}>{f.name}</div>
+                        <div>status:{f.status}</div>
                     </div>
-                </div>
+                    <div>
+                        <div>country:{f.location?.country}</div>
+                        <div>city:{f.location?.city}</div>
+                    </div>
 
-                <div className={s.NameStatus}>
-                    <div className={s.name}>{f.name}</div>
-                    <div>status:{f.status}</div>
-                </div>
-                <div>
-                    <div>country:{f.location?.country}</div>
-                    <div>city:{f.location?.city}</div>
-                </div>
-
-            </div>)
+                </div>)
             }
         </div>
     )
+
+
 }
 
 export default Users
