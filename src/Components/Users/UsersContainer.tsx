e0527f1dc4a82f199cbafaf507241ea7928f8c5e
Vladon79 <vla3ik@gmail.com>
2021-11-30 12:18:23 +0300

    57_commit

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -1,63 +1,71 @@
 import { connect } from 'react-redux';
 import { AppStoreType } from '../../redux/redux-store';
-import {  AddUsersAC, RemoveUsersAC, SetCurrentPageAC, SetTotalUsersCountAC, SetUsersAC,} from '../../redux/users-reduser';
+import { AddUsersAC, RemoveUsersAC, SetCurrentPageAC, SetTotalUsersCountAC, SetUsersAC, ToggleIsFetchingAC, } from '../../redux/users-reduser';
 import axios from "axios";
 import React from "react";
 import { UsersType } from "../../types/types";
 import Users from "./Users";
+import Preloader from '../common/Preloader/Preloader';
 
 type UsersPageType = {
     users: Array<UsersType>
     pageSize: number
     totalUsersCount: number
     currentPage: number
+    isFetching: boolean
     AddUsers: (friendsID: number) => void
     RemoveUsers: (friendsID: number) => void
     SetUsers: (friends: Array<UsersType>) => void
     setCurrentPage: (currentPage: number) => void
     SetTotalUsersCount: (totalUsersCount: number) => void
+    ToggleIsFetching: (isFetching: boolean) => void
 }
 class UsersContainer extends React.Component<UsersPageType>{
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+        this.props.ToggleIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.ToggleIsFetching(false)
+                this.props.SetUsers(response.data.items)
+                this.props.SetTotalUsersCount(100)
 
-            this.props.SetUsers(response.data.items)
-            this.props.SetTotalUsersCount(100)
-
-        });
+            });
     }
     onPageChange = (pageNumber: number) => {
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-
-            this.props.SetUsers(response.data.items)
-        });
+        this.props.ToggleIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.ToggleIsFetching(false)
+                this.props.SetUsers(response.data.items)
+            });
     }
-
-
-
     render() {
-        return < Users
-            totalUsersCount={this.props.totalUsersCount}
-            pageSize={this.props.pageSize}
-            currentPage={this.props.currentPage}
-            onPageChange={this.onPageChange}
-            RemoveUsers={this.props.RemoveUsers}
-            AddUsers={this.props.AddUsers}
-            users={this.props.users}
-        />
 
+        return <>
+            {this.props.isFetching ? <Preloader /> : null}
+            < Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChange={this.onPageChange}
+                RemoveUsers={this.props.RemoveUsers}
+                AddUsers={this.props.AddUsers}
+                users={this.props.users}
+            />
+        </>
     }
 }
 
 const mapStateToProps = (state: AppStoreType) => {
-    
+
     return {
-        users: state.usersPage.users , 
+        users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,  
-        currentPage:state.usersPage.currentPage
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching
     }
 }
 
@@ -65,20 +73,23 @@ const mapDispachToProps = (dispatch: any) => {
     return {
         AddUsers: (usersID: number) => {
             dispatch(AddUsersAC(usersID))
-            
+
         },
         RemoveUsers: (usersID: number) => {
             dispatch(RemoveUsersAC(usersID))
         },
         SetUsers: (users: Array<UsersType>) => {
-           
+
             dispatch(SetUsersAC(users))
         },
-        setCurrentPage:(currentPage:number)=>{
+        setCurrentPage: (currentPage: number) => {
             dispatch(SetCurrentPageAC(currentPage))
         },
-        SetTotalUsersCount:(totalUsersCount: number) =>{
+        SetTotalUsersCount: (totalUsersCount: number) => {
             dispatch(SetTotalUsersCountAC(totalUsersCount))
+        },
+        ToggleIsFetching: (isFetching: boolean) => {
+            dispatch(ToggleIsFetchingAC(isFetching))
         }
     }
 }
