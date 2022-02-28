a50bf5a98679322c00ff5d038f7e7ce5c55c0f2a
Vladon79 <vla3ik@gmail.com>
2022-02-28 14:21:40 +0300

    chrome_extensions

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,12 +1,11 @@
-import { dialogsReduser } from './dialogs-reduser';
-import { profileReducer } from './profile-reducer';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import {  usersReducer } from './users-reducer';
-import { authReducer } from './auth-reduser';
-import  thunkMiddleware from 'redux-thunk';
-import { reducer as formReducer } from 'redux-form'
-import { appReducer } from './app-reducer';
-
+import {dialogsReduser} from './dialogs-reduser';
+import {profileReducer} from './profile-reducer';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import {usersReducer} from './users-reducer';
+import {authReducer} from './auth-reduser';
+import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form'
+import {appReducer} from './app-reducer';
 
 const reducers = combineReducers({
     postPage: profileReducer,
@@ -17,9 +16,15 @@ const reducers = combineReducers({
     app: appReducer
 })
 
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const reduxStore = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
 export type AppStoreType = ReturnType<typeof reducers>
 
-const reduxStore = createStore(reducers, applyMiddleware(thunkMiddleware));
-
+//@ts-ignore
+window.__store__ = reduxStore
+// const reduxStore = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default reduxStore
